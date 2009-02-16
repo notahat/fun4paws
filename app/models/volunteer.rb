d1c9924b25f3864f861b387a8e8c30032937327d
Pete Yandell <pete@notahat.com>
2009-02-16 12:33:13 +1100

    Simplified the new volunteer form using a form builder.

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,9 @@
 class Volunteer < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name
+  validates_presence_of :address_1, :suburb
+  validates_format_of :postcode, :with => /^A\d\d\d\d^Z/
+  
+  wraps_attribute :mobile_phone, PhoneNumber
+  wraps_attribute :home_phone,   PhoneNumber
+  wraps_attribute :work_phone,   PhoneNumber
 end
