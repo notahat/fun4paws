92666fd01e468b83776cdfbe3dfd58cd3d9975cc
Pete Yandell <pete@notahat.com>
2009-02-16 12:48:21 +1100

    Added validation to ensure at least one phone number is entered for a volunteer.

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,8 +2,14 @@ class Volunteer < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
   validates_presence_of :address_1, :suburb
   validates_format_of :postcode, :with => /^A\d\d\d\d^Z/
+  wraps_attribute :mobile_phone, PhoneNumber, :allow_blank => true
+  wraps_attribute :home_phone,   PhoneNumber, :allow_blank => true
+  wraps_attribute :work_phone,   PhoneNumber, :allow_blank => true
+  validate :must_have_at_least_one_phone_number
   
-  wraps_attribute :mobile_phone, PhoneNumber
-  wraps_attribute :home_phone,   PhoneNumber
-  wraps_attribute :work_phone,   PhoneNumber
+  def must_have_at_least_one_phone_number
+    if mobile_phone.blank? && home_phone.blank? && work_phone.blank?
+      errors.add_to_base("You must provide at least one of mobile phone, home phone, or work phone")
+    end
+  end
 end
